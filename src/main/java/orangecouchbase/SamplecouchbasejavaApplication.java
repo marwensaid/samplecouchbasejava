2ff55d68c94e09d1d8db671883aa74372da8e19d
Marwen SAIDI <marwen.saidi@orange.com>
2015-09-23 09:57:11 +0200

    feat (samplecouchbasejava) generate project aspect and repo -- MS

---
+++ src/main/java/orangecouchbase/SamplecouchbasejavaApplication.java
@@ -0,0 +1,12 @@
+package orangecouchbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplecouchbasejavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamplecouchbasejavaApplication.class, args);
+    }
+}
---
+++ src/test/java/orangecouchbase/SamplecouchbasejavaApplicationTests.java
@@ -0,0 +1,16 @@
+package orangecouchbase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SamplecouchbasejavaApplication.class)
+public class SamplecouchbasejavaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
